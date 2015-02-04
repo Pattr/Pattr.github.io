0ce4d990b7fd1bb2a3c6044e70b7ce9d8683a4af
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-02-04 23:01:24 +0100

    Add placeholders in e2e test

--- test/e2e/HomePage.js
+++ test/e2e/HomePage.js
@@ -4,11 +4,35 @@
 	describe('Home Page ', function() {
 
 		beforeEach(function () {
-			browser.get('/#/');
+			browser.get('/');
 		});
 
 		it('should display the "Pattr" h1 title', function(){
 			expect(element(by.tagName('h1')).getInnerHtml()).toEqual('Pattr');
+		});
+
+		it('should contain the copyright paragrph', function(){
+			//TODO
+		});
+
+		it('should contain the About Pattr link', function(){
+			//TODO
+		});
+
+		it('should contain the Contact link', function(){
+			//TODO
+		});
+
+		it('should contain a <h1> with the classe CSS home', function(){
+			//TODO
+		});
+
+		it('should contain the Github link', function(){
+			//TODO
+		});
+
+		it('should contain the Twitter link', function(){
+			//TODO
 		});
 	});
 })();
